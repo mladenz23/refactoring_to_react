9be3cbe82a0484ce8db23bbd24a5a05ec3af8a7e
mladenz23 <mladen.zeljkovic@gmail.com>
2025-04-09 20:32:43 +0200

    Add minor changes to the style

--- index.js
+++ index.js
@@ -18,3 +18,38 @@ const observer = new IntersectionObserver(
 sections.forEach(section => {
   observer.observe(section);
 });
+
+function scroll() {
+  const navbar = document.querySelector('#navbar');
+  let isScrolled = false;
+
+  navbar.addEventListener('mouseover', () => {
+    if (isScrolled) {
+      navbar.style.opacity = '1';
+      navbar.style.transition = 'opacity 0.5s';
+    }
+  });
+
+  navbar.addEventListener('mouseout', () => {
+    if (isScrolled) {
+      navbar.style.opacity = '0.8';
+      navbar.style.transition = 'opacity 0.5s';
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop;
+
+    if (scrollTop > 70) {
+      navbar.style.opacity = '0.8';
+      navbar.style.transition = 'opacity 0.5s';
+      isScrolled = true;
+    } else {
+      navbar.style.opacity = '1';
+      navbar.style.transition = 'opacity 0.5s';
+      isScrolled = false;
+    }
+  });
+}
+
+scroll();
