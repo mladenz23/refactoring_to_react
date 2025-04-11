17ab04c38ec7baf877e8b8b4caad39405666954c
mladenz23 <mladen.zeljkovic@gmail.com>
2025-04-11 17:57:27 +0200

    Restructure a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import fbLogo from '../assets/icons/fbLogo.png';
 import fbLogoOff from '../assets/icons/fbLogoOff.png';
 import instLogo from '../assets/icons/instLogo.png';
@@ -6,44 +7,49 @@ import instLogoOff from '../assets/icons/instLogoOff.png';
 import twitLogo from '../assets/icons/twitLogo.png';
 import twitLogoOff from '../assets/icons/twitLogoOff.png';
 
-export default function Footer() {
-  const [fb, setFb] = useState(fbLogo);
-  const [inst, setInst] = useState(instLogo);
-  const [twit, setTwit] = useState(twitLogo);
+const images = [
+  {
+    image: fbLogo,
+    imageOff: fbLogoOff,
+    alt: 'Facebook Logo',
+    link: '#',
+  },
+  {
+    image: instLogo,
+    imageOff: instLogoOff,
+    alt: 'Instagram Logo',
+    link: '#',
+  },
+  {
+    image: twitLogo,
+    imageOff: twitLogoOff,
+    alt: 'Twitter Logo',
+    link: '#',
+  },
+];
 
+export default function Footer() {
   return (
     <footer
       id='contactLinks'
       className='bg-[#ddd] flex items-center justify-evenly py-12'
     >
       <div id='links' className='w-[200px] flex justify-between'>
-        <a href=''>
-          <img
-            className='w-[50px]'
-            src={fb}
-            alt='Facebook Logo'
-            onMouseEnter={() => setFb(fbLogoOff)}
-            onMouseLeave={() => setFb(fbLogo)}
-          />
-        </a>
-        <a href=''>
-          <img
-            className='w-[50px]'
-            src={inst}
-            alt='Instagram Logo'
-            onMouseEnter={() => setInst(instLogoOff)}
-            onMouseLeave={() => setInst(instLogo)}
-          />
-        </a>
-        <a href=''>
-          <img
-            className='w-[50px]'
-            src={twit}
-            alt='Twitter Logo'
-            onMouseEnter={() => setTwit(twitLogoOff)}
-            onMouseLeave={() => setTwit(twitLogo)}
-          />
-        </a>
+        {images.map((imageGroup, i) => {
+          const [hover, setHover] = useState(false);
+
+          return (
+            <a href={imageGroup.link} key={i}>
+              <img
+                className='w-[50px]'
+                src={hover ? imageGroup.imageOff : imageGroup.image}
+                alt={imageGroup.alt}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+              />
+            </a>
+          );
+        })}
       </div>
       <div id='arr'>&copy; 2024 BMI Tracker. All rights reserved.</div>
     </footer>
