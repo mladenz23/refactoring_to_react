bfe9d142f15c2aca5e47de969d17bc8025700901
mladenz23 <mladen.zeljkovic@gmail.com>
2025-04-11 19:59:53 +0200

    Add minor adjustments

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -13,14 +13,25 @@ export default function Interface() {
         </div>
         <div id='interfaceDesc' className='w-[57%]'>
           <h1 className='text-7xl text-[var(--orange)] mb-4'>
-          Ergonomic
+            Ergonomic
             <br />
             <span>Interface</span>
           </h1>
           <p className='text-[var(--white)] w-[55%]'>
-          Our ergonomic interface plays a key role in ensuring an efficient and pleasant user experience. It features thoughtfully placed elements that allow for intuitive navigation and easy use. Additionally, our interface is fully responsive across all devices, ensuring optimal readability and usability on screens of all sizes. Clear organization of information, along with consistent use of colors, typography, and icons, contributes to easy recognition and understanding of the interface. Overall, our interface provides a smooth and enjoyable user experience that promotes productivity and user satisfaction.
+            Our ergonomic interface plays a key role in ensuring an efficient
+            and pleasant user experience. It features thoughtfully placed
+            elements that allow for intuitive navigation and easy use.
+            Additionally, our interface is fully responsive across all devices,
+            ensuring optimal readability and usability on screens of all sizes.
+            Clear organization of information, along with consistent use of
+            colors, typography, and icons, contributes to easy recognition and
+            understanding of the interface. Overall, our interface provides a
+            smooth and enjoyable user experience that promotes productivity and
+            user satisfaction.
           </p>
-          <button id='interfaceButton' className={btn}>TRY NOW</button>
+          <button id='interfaceButton' className={btn}>
+            TRY NOW
+          </button>
         </div>
       </div>
     </section>
